5c75e309d480576d943003d5ca434d3bf1a2b4ab
dnyanesh181999 <dnyaneshdeshmukh506@gmail.com>
2024-05-22 15:52:15 +0530

    Commit by pranav deshmukh ----Validations for API Added

--- employeecrudapp_statuscode_ExceptionHandling_DataConversion/src/main/java/com/example/demo/model/Product.java
+++ employeecrudapp_statuscode_ExceptionHandling_DataConversion/src/main/java/com/example/demo/model/Product.java
@@ -1,9 +1,16 @@
 package com.example.demo.model;
 
+
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,7 +26,11 @@ public class Product {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Integer productId;
+@NotEmpty()
+@Size(min = 4,message = "Minimum 4 charector")
 private String productName;
+@NotEmpty(message = "Not empty")
 private String productType;
+@NotNull(message = "NOT BE Null")
 private Double productPrice;
 }
